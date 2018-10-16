f3e096463ace1e66f77d975bed03bbbead571b26
justcallmebeef <bethanymitch10@gmail.com>
2018-10-16 10:54:29 -0600

    updated css to create margins around cards and nav bar

--- src/components/Card.js
+++ src/components/Card.js
@@ -8,6 +8,7 @@ const Card = (props) => {
             <h5 className="card-title">{props.book.title}</h5>
             <h6 className="card-subtitle mb-2 text-muted">{props.book.author}</h6>
             <p className="card-text">{props.book.description}</p>
+            <a href="#" class="btn btn-primary">Add to Cart</a>
             </div>
         </div>
     )
